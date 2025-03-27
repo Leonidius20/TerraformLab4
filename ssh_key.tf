54e4ec736f4625fc1090efa4011ce2ea4563caa5
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2025-03-27 19:52:30 +0200

    removed commented out code

--- ssh_key.tf
+++ ssh_key.tf
@@ -2,8 +2,3 @@ resource "aws_key_pair" "leonid" {
     key_name_prefix = "leonid"
     public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGcfoBwfHL92t1LnquBKLe8JbglmQiEdAscpPaHGh3bq db"
 }
-
-#resource "aws_key_pair" "teacher" {
-#    key_name_prefix = "teacher"
-#    public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkAhvfRyUvgsUwENIds1a/4OvuHQgLki8K1EzPltl5M i.ilin@iilin-pro14.local"
-#}
